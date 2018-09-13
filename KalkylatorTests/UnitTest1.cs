9404672415a88fc16aa9de3caab31a6f9a17276f
oxygeneiv <oxygeneiv@hotmail.com>
2018-09-13 15:44:23 +0200

    Add project files.

---
+++ KalkylatorTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KalkylatorTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
